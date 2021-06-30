0c51490309476adb8a442baf47896425b83ca55b
Rosanne Timmerman <rosannetimmerman@hotmail.nl>
2021-06-30 11:45:00 +0100

    created function that returns random message based on three pieces of data

--- mixedMessages.js
+++ mixedMessages.js
@@ -6,10 +6,28 @@ thirdMsg = ["chocolate", "pears", "fake friends", "deer", "instagram"]
 
 pickFirst = () => {
     randomNr = Math.floor(Math.random() * 4);
-    console.log(randomNr);
+    //console.log(randomNr);
     return firstMsg[randomNr];
 }
 
+pickSecond = () => {
+    randomNr = Math.floor(Math.random() * 4);
+    //console.log(randomNr);
+    return secondMsg[randomNr];
+}
 
-console.log(pickFirst())
+pickThird = () => {
+    randomNr = Math.floor(Math.random() * 5);
+    //console.log(randomNr);
+    return thirdMsg[randomNr];
+}
+
+createMsg = () => {
+    return `${pickFirst()} ${pickSecond()} ${pickThird()}`
+}
+
+console.log(createMsg());
+//console.log(pickSecond());
+//console.log(pickThird());
+
 
