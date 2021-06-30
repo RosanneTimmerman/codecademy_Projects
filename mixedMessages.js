0718e6d37bdb1741d24e87d8a95ff17503754583
Rosanne Timmerman <rosannetimmerman@hotmail.nl>
2021-06-30 11:30:15 +0100

    added working function to pick first piece of data randomly

--- mixedMessages.js
+++ mixedMessages.js
@@ -4,10 +4,12 @@ secondMsg = ["hate", "loves", "despices", "believe in"]
 thirdMsg = ["chocolate", "pears", "fake friends", "deer", "instagram"]
 
 
-pickFirst() {
-    randomNr = Math.random() * 4
+pickFirst = () => {
+    randomNr = Math.floor(Math.random() * 4);
     console.log(randomNr);
     return firstMsg[randomNr];
 }
 
+
 console.log(pickFirst())
+
